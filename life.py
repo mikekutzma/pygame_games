b25b14cb6eaf73eaf3d2fb445346cdd7d14d7eb6
Mike Kutzma <michaelkutzma@gmail.com>
2017-06-14 12:44:30 -0400

    use numpy to simplify updates

--- life.py
+++ life.py
@@ -3,16 +3,18 @@
 import pygame
 import random
 import time
+import numpy as np
+import math
 
-black = 	[0, 0, 0	]
-white = 	[255, 255, 255	]
-green  = 	[0, 255, 0  	]
-red = 		[255, 0, 0      ]
-blue = 		[0, 0, 255     	]
-yellow = 	[255, 255, 0	]
-pink = 		[255, 0, 127	]
-orange = 	[255, 128, 0	]
-purple = 	[102, 0, 204	]
+black =         [0, 0, 0        ]
+white =         [255, 255, 255  ]
+green  =        [0, 255, 0      ]
+red =           [255, 0, 0      ]
+blue =          [0, 0, 255      ]
+yellow =        [255, 255, 0    ]
+pink =          [255, 0, 127    ]
+orange =        [255, 128, 0    ]
+purple =        [102, 0, 204    ]
 
 
 pygame.init()
@@ -28,8 +30,8 @@ gen = 3
 speed = 5.0
 inc = 1.0
 
-numrows = 60
-numcols = 100
+numrows = 10
+numcols = 10
 
 sizevert = margin + ((margin + height) * numrows)
 sizehorz = margin + ((margin + width) * numcols)
@@ -41,17 +43,32 @@ pygame.display.set_caption("The Game of Life")
 
 clock = pygame.time.Clock()
 
-grid = []
-for row in range(numrows):
-	grid.append([])
-	for column in range(numcols):
-		grid[row].append(0)
-gridaux = []
-for row in range(numrows):
-	gridaux.append([])
-	for column in range(numcols):
-		gridaux[row].append(0)
+def grid_init_blank():
+    grid = np.zeros((numrows,numcols))
+    return grid
 
+def grid_init_rand():
+    grid = np.random.random((numrows,numcols))
+    return grid
+
+def get_neighbors(grid,x,y):
+    nbrs = grid[max(0,x-1):min(numrows,x+2),max(0,y-1):min(numcols,y+2)]
+    total = np.sum(nbrs) - grid[x][y]
+    return total
+
+def step(x,n):
+    if(x==0):
+        if(n==gen):
+            return 1
+    elif(x==1):
+        if((n>iso) & (n<ovrc)):
+            return 1
+    else:
+        return -1
+    return 0
+
+grid = grid_init_blank()
+gridaux = grid_init_blank()
 
 done = False
 play = False
@@ -61,9 +78,8 @@ while done == False:
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.MOUSEBUTTONDOWN:
-            grid[x][y] +=1
-            if grid[x][y] == 2:
-                grid[x][y] = 0
+            grid[x][y] = (grid[x][y]+1)%2
+            get_neighbors(grid,x,y)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 done = True
@@ -78,50 +94,17 @@ while done == False:
             if event.key == pygame.K_SPACE:
                 play = (play==False)
             if event.key == pygame.K_BACKSPACE:
-                for row in range(numrows):
-                    for column in range(numcols):
-                        grid[row][column] = 0
+                grid = grid_init_blank()
+                gridaux = grid_init_blank()
+            if event.key == pygame.K_r:
+                grid = grid_init_rand()
+                gridaux = grid_init_blank()
     if play == True:
-        for row in range(numrows):
-            for column in range(numcols):
-                sum = 0
-                if row > 0:
-                    if column > 0:
-                        if grid[row-1][column-1] == 1:
-                            sum = sum+1
-                    if column < numcols-1:
-                        if grid[row-1][column+1] == 1:
-                            sum = sum+1
-                    if grid[row-1][column] == 1:
-                        sum = sum+1
-                if row < numrows-1:
-                    if column > 0:
-                        if grid[row+1][column-1] == 1:
-                            sum = sum+1
-                    if column < numcols-1:
-                        if grid[row+1][column+1] == 1:
-                            sum = sum+1
-                    if grid[row+1][column] == 1:
-                        sum = sum+1
-                if column > 0:
-                    if grid[row][column-1] == 1:
-                        sum = sum+1
-                if column < numcols-1:
-                    if grid[row][column+1] == 1:
-                        sum = sum+1
-                if grid[row][column] == 1:
-                    if sum <= iso or sum >= ovrc:
-                        gridaux[row][column] = 0
-                    elif sum > iso and sum < ovrc:
-                        gridaux[row][column] = 1
-                if grid[row][column] == 0:
-                    if sum != gen:
-                        gridaux[row][column] = 0
-                    elif sum == gen:
-                        gridaux[row][column] = 1
-        for row in range(numrows):
-            for column in range(numcols):
-                grid[row][column] = gridaux[row][column]
+        for r in range(numrows):
+            for c in range(numcols):
+                n = get_neighbors(grid,r,c)
+                gridaux[r][c] = step(grid[r][c],n)
+        grid = gridaux.copy()
         time.sleep(1.0/speed)
 
 
@@ -133,7 +116,7 @@ while done == False:
 
     tot = 0
     for row in range(numrows):
-        for column in range(numcols):	
+        for column in range(numcols):   
             color = white
             if grid[row][column] == 1:
                 color = orange                        #Color Change
